35c3441bf7cdbf85d48f1572956743b0d22ef6d8
Dr Nic Williams <drnicwilliams@gmail.com>
2010-11-22 21:07:34 -0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HudsonServer::Application.config.secret_token = '10c6120d063b576108071bfcf10d9ead19da33a3addef0d979afc2349b46aab274beb2e5a88544559151de758c50d97b5c253e447c0916fb2181a6dba28329c7'
